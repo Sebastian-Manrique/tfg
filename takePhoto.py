4cd3c4fd8c3c752de04cbc54cddc4a3b3ba91e6d
WildRepublicMini <smanriquemontiel@gmail.com>
2025-03-11 19:02:39 +0100

    Now the script can read barcodes and qr codes

--- takePhoto.py
+++ takePhoto.py
@@ -1,4 +1,6 @@
 import cv2
+from pyzbar import pyzbar
+import os
 
 # Iniciar la captura de video desde la webcam (cambia 0 si tienes varias cámaras)
 cap = cv2.VideoCapture(0)
@@ -12,9 +14,38 @@ if not cap.isOpened():
 ret, frame = cap.read()
 
 if ret:
+
+    # Buscar el siguiente número disponible, para guardar la imagen
+    contador = 1
+    while os.path.exists(os.path.join("img", f"foto{contador}.jpg")):
+        contador += 1
+
+    # Nombre final de la nueva imagen
+    nuevo_nombre = os.path.join("img", f"foto{contador}.jpg")
+
     # Guardar la imagen
     cv2.imwrite("img/foto.jpg", frame)
-    print("Foto guardada como 'foto.jpg'")
+    os.rename("img/foto.jpg", nuevo_nombre)
+    print(f"Foto guardada como {nuevo_nombre}")
+
+    # Leer el código QR
+    img = cv2.imread(nuevo_nombre)
+
+    barcodes = pyzbar.decode(img)
+
+    for barcode in barcodes:
+        x, y, w, h = barcode.rect
+        cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 255), 4)
+        bdata = barcode.data.decode("utf-8")
+        btype = barcode.type
+        text = f"{bdata}, {btype}"
+        cv2.putText(img, text, (x, y-10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+    cv2.imshow("foto", img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
 else:
     print("Error: No se pudo capturar la imagen")
 
