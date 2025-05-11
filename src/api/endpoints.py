72fde373493fe386bf17180b56cf92617302ad41
WildRepublicMini <smanriquemontiel@gmail.com>
2025-05-12 00:23:29 +0200

    Added
    Part of the translation for the web
    
    A very little API for restarting the camera (still working on it)

--- src/api/endpoints.py
+++ src/api/endpoints.py
@@ -1,27 +1,68 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response, jsonify, after_this_request
+import cv2
 
 app = Flask(__name__)
-
 
 translations = {
     'es': {
         'title': 'Backend de Escáner de Botellas',
         'status': 'Estado de la máquina',
         'manual_scan': 'Iniciar escaneo manual',
+        'video_actual': 'Video actual de la cámara',
+        'ver_camara': 'Ver cámara en linea',
         'restart': 'Reiniciar máquina',
     },
     'en': {
         'title': 'Bottle Scanner Backend',
         'status': 'Machine Status',
         'manual_scan': 'Start Manual Scan',
+        'video_actual': 'Actual camera video',
+        'ver_camara': 'View camera online',
         'restart': 'Restart Machine',
     }
 }
 
-@app.route('/camera')
-def hello():
-    hora_fun_flask = "12:79"
+camera = cv2.VideoCapture(0)  # Open the default camera (0)
+
+
+def camera_frames():
+    while True:
+        isEnable, frame = camera.read()
+        if not isEnable:
+            print("Failed to capture image")
+            break
+        else:
+            # Encode the frame as JPEG
+            response, jpeg = cv2.imencode('.jpg', frame)
+            frame = jpeg.tobytes()
+
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+@app.route('/')
+def index():
+    hour_fun_flask = "12:79"
     camera_fun_flask = True
-    return render_template("camera.html", hora=hora_fun_flask, cameraBool=camera_fun_flask)
+    return render_template("camera.html", hora=hour_fun_flask, cameraBool=camera_fun_flask, lang=translations['es'])
+
+
+@app.route('/en')
+def index_en():
+    hour_fun_flask = "12:79"
+    camera_fun_flask = True
+    return render_template("camera.html", hora=hour_fun_flask, cameraBool=camera_fun_flask, lang=translations['en'])
+
+
+@app.route('/video')
+def camera_stream():
+    return Response(camera_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+@app.route('/restart-camera', methods=['POST'])
+def restart():
+    camera.release()
+    return jsonify({"status": "Camera restarted"})
+
 
 app.run(host="0.0.0.0", port=5001, debug=True)
---
+++ src/api/static/js/script.js
@@ -0,0 +1,25 @@
+function toggleVideo() {
+  const video = document.getElementById("video-stream");
+  const icon = document.getElementById("icon-stream");
+  const button = document.getElementById("btnWatch");
+
+  if (video.style.display === "none") {
+    video.style.display = "block";
+    icon.style.display = "none";
+    button.innerHTML = "Dejar de ver";
+    button.style.backgroundColor = "red";
+  } else {
+    video.style.display = "none";
+    icon.style.display = "block";
+    button.innerHTML = "Ver camara en linea";
+    button.style.backgroundColor = "#008080";
+  }
+}
+
+function restart() {
+  fetch("http://127.0.0.1:5001/restart-camera", {
+    method: "POST",
+  })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+}
